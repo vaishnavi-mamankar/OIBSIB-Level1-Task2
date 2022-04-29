7657e1ecaebdd61f2c53f3d15a9040da17c76dd6
Vaishnavi Mamankar <91334711+vaishnavi-mamankar@users.noreply.github.com>
2022-04-29 10:35:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# OIBSIP-Level-1-Task-2
+
+# Intern at Oasis Infobyte #AprilBatch
+
+Web Development and Designing
+
+Level 1 :- 
+
+Task 2: Portfolio : https://karank28.github.io/OIBSIP-Level-1-Task-2/
+
+Languages: HTML, CSS, Javascript
+
+LinkedIn: https://www.linkedin.com/in/karan-mistry-4222951a5/
---
+++ script.js
@@ -0,0 +1,48 @@
+//javascript for navigation bar effects on scroll 
+  window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+  });
+  
+  //javascript for responsive navigation sidebar menu
+  const open = document.querySelector("#open");
+  const close = document.querySelector("#close");
+  const navigation = document.querySelector(".navigation");
+
+  open.addEventListener("click", () => {
+      navigation.classList.add("active");
+  });
+
+  close.addEventListener("click", () => {
+      navigation.classList.remove("active");
+  });
+  
+  //javascript for scroll to top button
+  const scrollBtn = document.querySelector(".scrollToTop-btn");
+  
+  window.addEventListener("scroll", function(){
+    scrollBtn.classList.toggle("active", window.scrollY > 500);
+  });
+  
+  //javascript for scroll back to top on click scroll-to-top button
+  scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+  
+  //javascript for reveal website elements on scroll
+  window.addEventListener("scroll", reveal);
+  
+  function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for(var i = 0; i < reveals.length; i++){
+      var windowHeight = window.innerHeight;
+      var revealTop = reveals[i].getBoundingClientRect().top;
+      var revealPoint = 50;
+  
+      if(revealTop < windowHeight - revealPoint){
+        reveals[i].classList.add("active");
+      }
+    }
+  }
